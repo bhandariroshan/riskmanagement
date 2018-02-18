04a7189b30313a749d8a921ef189efc31832b806
Roshan <brishi98@gmail.com>
2018-02-18 22:10:07 +0545

    changes

--- config/settings/base.py
+++ config/settings/base.py
@@ -237,7 +237,7 @@ WSGI_APPLICATION = 'vuejsapp.config.wsgi.application'
 # ------------------------------------------------------------------------------
 # See https://docs.djangoproject.com/en/dev/topics/auth/passwords/#using-argon2-with-django
 PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    # 'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
